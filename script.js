e7f94fbf3c1742159969dfd7b4d9e8c539e71123
ashkankhademi <ashkhademii@gmail.com>
2025-03-16 21:42:57 -0400

    added night mode toggle feature

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = themeToggle.querySelector('i');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    themeIcon.classList.remove('fa-moon');
+    themeIcon.classList.add('fa-sun');
+  } else {
+    themeIcon.classList.remove('fa-sun');
+    themeIcon.classList.add('fa-moon');
+  }
+});
